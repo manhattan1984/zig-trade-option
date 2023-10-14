dec14d5c98e9b9220d95742a17978d6d7719a042
manhattan1984 <mikkimanhattan@gmail.com>
2023-10-14 18:28:52 +0100

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,16 @@
+import Script from "next/script";
+
+export default function Head() {
+  return (
+    <>
+      <title>Zig Trade Option</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Zig Trade Option" />
+      <link rel="icon" href="/favicon.ico" />
+      <Script
+        src="//code.tidio.co/4ucrahysxueelosxvphsjmwevonxswjq.js"
+        async
+      ></Script>
+    </>
+  );
+}
